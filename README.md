6ed72da66c55cfcf57b3e4d2c286b317faf21bac
AVA Sharkawy <49260654+AvaSharkawy@users.noreply.github.com>
2019-10-14 11:20:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blueberry
+
+Welcome to the new open-source £100,000 diabetes project.
+
+In this I aim to create the entire applications and system for a new diabetes app that helps diabetics manage and have more fun managing their condition.
---
+++ Source/Blueberry.Desktop.WindowsApp.Bluetooth/GattService.cs
@@ -0,0 +1,54 @@
+﻿namespace Blueberry.Desktop.WindowsApp.Bluetooth
+{
+    /// <summary>
+    /// Details about a specific GATT Service
+    /// <seealso cref="https://www.bluetooth.com/specifications/gatt/services/"/>
+    /// </summary>
+    public class GattService
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The human-readable name for the service
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// The uniform identifier that is unique to this service
+        /// </summary>
+        public string UniformTypeIdentifier { get; }
+
+        /// <summary>
+        /// The 16-bit unsigned number for this service.
+        /// The Bluetooth GATT Service UUID contains this
+        /// </summary>
+        public ushort AssignedNumber { get; }
+
+        /// <summary>
+        /// The type of specification that this service is.
+        /// <seealso cref="https://www.bluetooth.com/specifications/gatt/"/>
+        /// </summary>
+        public string ProfileSpecification { get; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="uniformIdentifier"></param>
+        /// <param name="assignedNumber"></param>
+        /// <param name="profileSpecification"></param>
+        public GattService(string name, string uniformIdentifier, ushort assignedNumber, string profileSpecification)
+        {
+            Name = name;
+            UniformTypeIdentifier = uniformIdentifier;
+            AssignedNumber = assignedNumber;
+            ProfileSpecification = profileSpecification;
+        }
+
+        #endregion
+    }
+}
